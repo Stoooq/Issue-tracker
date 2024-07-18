cc1053a7a18cbf567c83332ceecdf7a9f19962df
Milosz Glowacki <milosz.glow@gmail.com>
2024-07-18 12:17:41 +0200

    build summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Pagination from "./components/Pagination";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
 
-interface Props {
-  searchParams: { page: string }
-}
 
-export default function Home({ searchParams }: Props) {
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' }
+  })
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' }
+  })
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' }
+  })
+
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
+    <IssueSummary open={open} inProgress={inProgress} closed={closed} />
   );
 }
